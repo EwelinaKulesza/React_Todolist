6d9e62cfb42bf9aea60d2f12ea6c9366df236d17
Ewelina Kulesza <cegla00@gamil.com>
2021-02-19 19:55:24 +0100

    skeleton of an app created

--- todolst/src/App.js
+++ todolst/src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 //Importowanie komponentow
 import Form from './components/Form';
+import TodoList from './components/TodoList';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <h1>Ewelina's ToDo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
