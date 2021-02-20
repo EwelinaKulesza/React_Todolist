ec8686aa9fd904b6ddd3ec5bda3f27756d8f08de
Ewelina Kulesza <cegla00@gamil.com>
2021-02-20 09:26:27 +0100

    states are selected

--- todolst/src/components/Form.js
+++ todolst/src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
     //Tu mogę wpisać kod java script i wrzucać funkcje
     const setInputTextHandler = (e) => {
         console.log(e.target.value);  //loguje tu event
@@ -12,15 +12,24 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
             ...todos, {text: inputText, completed: false,id: Math.floor(Math.random()*100000)}
         ]);
         setInputText("");
+    };
+    const statusHandler = (e) => {
+        // console.log(e.target.value);
+        setStatus(e.target.value);
     }
     return(
  <form>   
-      <input value={inputText} onChange={setInputTextHandler} type="text"  className="todo-input"/>   {/**to miejsce gdzie wpisuję zadania */}
+      <input 
+      value={inputText} 
+      onChange={setInputTextHandler} 
+      type="text" 
+      className="todo-input"
+      />
      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
      </button>
      <div className="select">
-         <select name="todos" className="filter-todo">
+         <select onChange={statusHandler} name="todos" className="filter-todo">
              <option value="all">All</option>
              <option valie="completed">Completed</option>
              <option value="uncompleted">Uncompleted</option>
