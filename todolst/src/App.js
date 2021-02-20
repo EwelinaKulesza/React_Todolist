ae983c1cad6d10c91b8a5c927debdb048865952c
Ewelina Kulesza <cegla00@gamil.com>
2021-02-20 10:48:21 +0100

    Sort works

--- todolst/src/App.js
+++ todolst/src/App.js
@@ -5,23 +5,27 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
-  //USE EFFECT
-  useEffect(() => {
-    console.log('hey')
-  }, [])
+
   //State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //USE EFFECT
+  useEffect(() => {
+    // console.log('hey')
+    filterHandler();
+  }, [todos, status])
+
   //Functions
   const filterHandler = () => {
     switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed ==true))
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
         break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed ==false))
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
         break;  
       default: 
         setFilteredTodos(todos);
@@ -35,13 +39,17 @@ function App() {
         <h1>Ewelina's ToDo List </h1>
       </header>
       <Form 
-      inputText={inputText} 
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText}
-      setStatus={setStatus}
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList 
+        filteredTodos={filteredTodos} 
+        setTodos={setTodos} 
+        todos={todos} 
+      />
     </div>
   );
 }
--- todolst/src/components/Form.js
+++ todolst/src/components/Form.js
@@ -31,7 +31,7 @@ const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
      <div className="select">
          <select onChange={statusHandler} name="todos" className="filter-todo">
              <option value="all">All</option>
-             <option valie="completed">Completed</option>
+             <option value="completed">Completed</option>
              <option value="uncompleted">Uncompleted</option>
          </select>
      </div>
