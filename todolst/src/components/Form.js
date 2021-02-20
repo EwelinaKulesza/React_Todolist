ea0e511e13d1d84dafc1a9da0a861afff2f07697
Ewelina Kulesza <cegla00@gamil.com>
2021-02-20 04:29:51 +0100

    ad todo form

--- todolst/src/components/Form.js
+++ todolst/src/components/Form.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const Form = ({ setInputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
     //Tu mogę wpisać kod java script i wrzucać funkcje
     const setInputTextHandler = (e) => {
         console.log(e.target.value);  //loguje tu event
         setInputText(e.target.value);
     }
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {text: inputText, completed: false,id: Math.floor(Math.random()*100000)}
+        ]);
+    }
     return(
  <form>   
-     to miejsce gdzie
       <input onChange={setInputTextHandler} type="text"  className="todo-input"/>   {/**to miejsce gdzie wpisuję zadania */}
-     <button className="todo-button" type="submit">
+     <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
      </button>
      <div className="select">
