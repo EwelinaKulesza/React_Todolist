ba52886cd59cb7dbd2db14bc8457d19f85b0c152
Ewelina Kulesza <cegla00@gamil.com>
2021-02-20 09:47:28 +0100

    use useEffect function

--- todolst/src/App.js
+++ todolst/src/App.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 //Importowanie komponentow
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  //USE EFFECT
+  useEffect(() => {
+    console.log('hey')
+  }, [])
+  //State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed ==true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed ==false))
+        break;  
+      default: 
+        setFilteredTodos(todos);
+        break;
+    }
+
+  }
   return (
     <div className="App">
       <header>
